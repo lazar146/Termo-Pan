4857961d6fc449af0b72bb7822e7ab780c6417cd
lazar146 <95591229+lazar146@users.noreply.github.com>
2022-12-14 10:46:19 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -194,12 +194,17 @@ var Greska = 0;
 var menu = document.querySelector(".menu");
 var menuItems = document.querySelectorAll(".menuItem");
 var hamburger= document.querySelector(".hamburger");
+var closeIcon= document.querySelector(".closeIcon");
 var menuIcon = document.querySelector(".menuIcon");
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
   } else {
     menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
   }
 }
 
